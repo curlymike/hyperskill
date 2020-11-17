6835c2ddaf9d2e31342adfe6e1cd54bcb86020b9
Michael <curlymike@gmail.com>
2020-11-17 18:02:21 +0300

    More tests

--- src/test/java/tictactoe/MiniMaxTest.java
+++ src/test/java/tictactoe/MiniMaxTest.java
@@ -22,8 +22,9 @@ public class MiniMaxTest {
                 {X, 0, X},
                 {0, O, O},
         };
-        assertThat(compute(field, X).getScore()).isEqualTo(10);
-        assertThat(compute(field, X).getCell()).isEqualTo(new Main.Cell(1, 1));
+        MiniMax.Result result = compute(field, X);
+        assertThat(result.getScore()).isEqualTo(10);
+        assertThat(result.getCell()).isEqualTo(new Main.Cell(1, 1));
     }
 
     @Test
@@ -33,17 +34,6 @@ public class MiniMaxTest {
                 {X, O, 0},
                 {O, 0, X},
         };
-//        int[][] field = {
-//                {O, X, X},
-//                {X, O, O},
-//                {O, 0, X},
-//        };
-//        int[][] field = {
-//                {O, X, 0},
-//                {X, O, O},
-//                {O, X, X},
-//        };
-        //field[2][1] = X;
         MiniMax.Result result = compute(field, X);
         assertThat(result.getScore()).isEqualTo(0);
         assertThat(result.getCell()).isEqualTo(new Main.Cell(0, 2));
@@ -81,6 +71,54 @@ public class MiniMaxTest {
         };
         assertThat(compute(field, X, MiniMax.MIN).getScore()).isEqualTo(-10);
         assertThat(compute(field, X, MiniMax.MAX).getScore()).isEqualTo(10);
+    }
+
+    @Test
+    public void compute_100() {
+        int[][] field = {
+                {0, 0, 0},
+                {X, O, 0},
+                {0, 0, 0},
+        };
+        MiniMax.Result result = compute(field, X);
+        //assertThat(result.getScore()).isEqualTo(10);
+        assertThat(result.getCell()).isEqualTo(new Main.Cell(1, 0));
+    }
+
+    @Test
+    public void compute_101() {
+        int[][] field = {
+                {0, 0, 0},
+                {X, O, 0},
+                {0, 0, 0},
+        };
+        MiniMax.Result result = compute(field, O);
+        //assertThat(result.getScore()).isEqualTo(10);
+        assertThat(result.getCell()).isEqualTo(new Main.Cell(0, 1));
+    }
+
+    @Test
+    public void compute_102() {
+        int[][] field = {
+                {0, X, 0},
+                {X, O, 0},
+                {0, O, 0},
+        };
+        MiniMax.Result result = compute(field, O);
+        //assertThat(result.getScore()).isEqualTo(10);
+        assertThat(result.getCell()).isEqualTo(new Main.Cell(0, 0));
+    }
+
+    @Test
+    public void compute_103() {
+        int[][] field = {
+                {X, X, 0},
+                {X, O, 0},
+                {O, O, 0},
+        };
+        MiniMax.Result result = compute(field, O);
+        //assertThat(result.getScore()).isEqualTo(10);
+        assertThat(result.getCell()).isEqualTo(new Main.Cell(0, 2));
     }
 
 }
