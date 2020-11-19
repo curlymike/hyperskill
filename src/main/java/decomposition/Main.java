ab01243c7c416047df25b0c303cfaa917bb36e5c
Michael <curlymike@gmail.com>
2020-11-19 23:16:43 +0300

    Cleaning up commented code

--- src/main/java/decomposition/Main.java
+++ src/main/java/decomposition/Main.java
@@ -6,22 +6,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-//        decomp2(1);
-//        System.out.println("------------");
-//        decomp2(2);
-//        System.out.println("------------");
-        //decomp2(5);
-        //decomp2(7);
-        //decomp2(4);
         int n = 5;
         for (String str : decomp(n, n)) {
             System.out.println(str);
         }
-//        System.out.println("------------");
-//        decomp2(4);
-//        System.out.println("------------");
-//        decomp2(5);
-//        System.out.println("------------");
     }
 
     public static List<String> decomp(int cap, int n) {
