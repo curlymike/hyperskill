d13b5e0289ecf5fe38e90ecd7f4ae2f575eb79cf
Michael <curlymike@gmail.com>
2020-11-19 23:04:52 +0300

    I think I got it!

--- src/main/java/decomposition/Main.java
+++ src/main/java/decomposition/Main.java
@@ -1,13 +1,22 @@
 package decomposition;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        decomp2(1);
-        System.out.println("------------");
-        decomp2(2);
-        System.out.println("------------");
-        decomp2(3);
+//        decomp2(1);
+//        System.out.println("------------");
+//        decomp2(2);
+//        System.out.println("------------");
+        //decomp2(5);
+        //decomp2(7);
+        //decomp2(4);
+        int n = 5;
+        for (String str : decomp(n, n)) {
+            System.out.println(str);
+        }
 //        System.out.println("------------");
 //        decomp2(4);
 //        System.out.println("------------");
@@ -15,29 +24,22 @@ public class Main {
 //        System.out.println("------------");
     }
 
-    public static void decomp2(int n) {
-        if (n == 1) {
-            System.out.print("1 ");
-        } else if (n == 2) {
-            System.out.print("1 1 ");
+    // Do I even need "cap" here?
+    public static List<String> decomp(int cap, int n) {
+        List<String> output = new ArrayList<>();
+        if (n > 1) {
+            for (int i = 1; i <= cap && i <= n; i++) {
+                for (String str : decomp(i, n - i)) {
+                    StringBuilder sb = new StringBuilder();
+                    output.add(sb.append(i).append(' ').append(str).toString());
+                }
+            }
+        } else if (n == 1) {
+            output.add("1");
         } else {
-            decomp2(n - 1);
-            System.out.print("1 ");
+            output.add(String.valueOf(n));
         }
+        return output;
     }
 
-//    public static int[] decomp(int n) {
-//        if (n == 1) {
-//            int[] arr = new int[1];
-//            arr[0] = 1;
-//            return arr;
-//        }
-//        if (n == 2) {
-//            int[] arr = new int[2];
-//            arr[0] = 1;
-//            arr[1] = 1;
-//            return arr;
-//        }
-//        return decomp(n - 1);
-//    }
 }
